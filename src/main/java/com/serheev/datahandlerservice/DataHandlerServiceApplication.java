f245e0a05c5efb6807fdb02266755ba523cab962
Serheev <yserheev@gmail.com>
2024-03-05 14:53:32 -0800

    Initialization

---
+++ src/main/java/com/serheev/datahandlerservice/DataHandlerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.serheev.datahandlerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataHandlerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataHandlerServiceApplication.class, args);
+    }
+
+}
