468dfe311d7799db4c99da74afb0951a8287a69e
Serheev <yserheev@gmail.com>
2024-03-05 19:35:29 -0800

    Add: PostgreSQL DB for saving received data and Liquibase

--- src/main/java/com/serheev/datahandlerservice/model/Data.java
+++ src/main/java/com/serheev/datahandlerservice/model/Data.java
@@ -1,5 +1,6 @@
 package com.serheev.datahandlerservice.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,14 +8,25 @@ import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name="data")
 @NoArgsConstructor
 @Setter
 @Getter
 @ToString
 public class Data {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     private Long sensorId;
     private LocalDateTime timestamp;
     private double measurement;
+
+    @Column(name = "type")
+    @Enumerated(value = EnumType.STRING)
     private MeasurementType measurementType;
 
     public enum MeasurementType {
--- src/main/java/com/serheev/datahandlerservice/service/KafkaDataServiceImpl.java
+++ src/main/java/com/serheev/datahandlerservice/service/KafkaDataServiceImpl.java
@@ -1,12 +1,19 @@
 package com.serheev.datahandlerservice.service;
 
 import com.serheev.datahandlerservice.model.Data;
+import com.serheev.datahandlerservice.repository.DataRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class KafkaDataServiceImpl implements KafkaDataService {
+    private final DataRepository dataRepository;
     @Override
     public void handle(Data data) {
-        System.out.println("Data is received: " + data.toString());
+        log.info("Data {} was received: ", data);
+        dataRepository.save(data);
     }
 }
