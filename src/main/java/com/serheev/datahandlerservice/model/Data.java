3b1c1adc1ebc9b30c020b777080054c551a29a2e
Serheev <yserheev@gmail.com>
2024-03-05 16:44:14 -0800

    Fix data receive format

--- src/main/java/com/serheev/datahandlerservice/model/Data.java
+++ src/main/java/com/serheev/datahandlerservice/model/Data.java
@@ -3,12 +3,14 @@ package com.serheev.datahandlerservice.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Setter
 @Getter
+@ToString
 public class Data {
     private Long sensorId;
     private LocalDateTime timestamp;
